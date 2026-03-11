03b50c3373c1d108a2184aa2a01071d4cddd7df2
TatiDT <tatiabarca12@email.com>
2026-03-11 17:10:08 -0300

    First advance

---
+++ app.py
@@ -0,0 +1,16 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/camiones')
+def camiones():
+    return render_template('camiones.html')
+@app.route('/mantenimiento')
+def mantenimiento():
+    return render_template('mantenimiento.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
